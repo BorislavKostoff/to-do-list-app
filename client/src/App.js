cc74c1d69036098f25f152ef2c06db70a8ea0aa4
birata81 <borislav.kostov81@gmail.com>
2024-08-17 18:37:09 +0300

    Added the Add new note button in main page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
+import { Box, IconButton, Stack } from '@mui/material'
+import NoteAddOutlinedIcon from '@mui/icons-material/NoteAddOutlined';
 
 function App() {
   return (
-    <div>
-      TEST
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-around",
+        marginTop: "10px"
+      }}
+    >
+      <Stack
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignContent: "center",
+          fontWeight: "bold",
+          fontSize: "6vh"
+        }}
+      >To-Do LISTS</Stack>
+      <IconButton onClick={null} size='large' color='primary'><NoteAddOutlinedIcon fontSize='large'/></IconButton>
+    </Box>
   );
 }
 
--- client/src/components/ButtonForm.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ButtonForm = () => {
-  return (
-    <div>ButtonForm</div>
-  )
-}
-
-export default ButtonForm
